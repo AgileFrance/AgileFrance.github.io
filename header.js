b4d2ca39b504e490b335d0da112e40f4bb1bb2f9
Colin Garriga-Salaün <colin@yaal.fr>
2017-03-07 22:44:08 +0100

    C'est beau ?

--- header.js
+++ header.js
@@ -7,5 +7,5 @@ document.write(
   '  <div class="nom_asso">                                                    \n' +
   '    <a href="http://agile-france.org">Association Agile France</a>          \n' +
   '  </div>                                                                    \n' +
-  '  <div class="logo_asso">▤ ▣ ▨</div>                                        \n' +
+  '  <div class="logo_asso"><img src="/logo.svg" height="150px" /></div>        \n' +
   '</header>');
